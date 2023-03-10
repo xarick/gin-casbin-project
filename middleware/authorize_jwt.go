a5455eefdd08acf03a84ab2ce9c6523de2635659
Bobirjon Tohirov <tokhirovbob@gmail.com>
2023-03-10 11:09:30 +0500

    add middleware

---
+++ middleware/authorize_jwt.go
@@ -0,0 +1,47 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"github.com/xarick/gin-casbin-project/utils"
+)
+
+// AuthorizeJWT -> to authorize JWT Token
+func AuthorizeJWT() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		const BearerSchema string = "Bearer "
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "No Authorization header found"})
+
+		}
+		tokenString := authHeader[len(BearerSchema):]
+
+		if token, err := utils.ValidateToken(tokenString); err != nil {
+
+			fmt.Println("token", tokenString, err.Error())
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "Not Valid Token"})
+
+		} else {
+
+			if claims, ok := token.Claims.(jwt.MapClaims); !ok {
+				ctx.AbortWithStatus(http.StatusUnauthorized)
+
+			} else {
+				if token.Valid {
+					ctx.Set("userID", claims["userID"])
+				} else {
+					ctx.AbortWithStatus(http.StatusUnauthorized)
+				}
+
+			}
+		}
+
+	}
+
+}
